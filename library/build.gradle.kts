82c81f8c607f747f4213178570007725b8b5a59f
jokuskay <tafanasyev@gmail.com>
2018-03-18 19:28:46 +0700

    add rxjava adapter dependency

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-	kotlin("jvm")
+    kotlin("jvm")
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib"))
+    compile(group = "com.squareup.retrofit2", name = "retrofit", version = "2.3.0")
+    compile(group = "com.squareup.retrofit2", name = "adapter-rxjava2", version = "2.3.0")
+    implementation(group = "com.squareup.retrofit2", name = "converter-gson", version = "2.3.0")
 }
