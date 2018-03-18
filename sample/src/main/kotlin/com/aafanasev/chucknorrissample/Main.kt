0c9381a6514a2961df0e9d511cf427ef6aaf3ad2
jokuskay <tafanasyev@gmail.com>
2018-03-18 19:30:30 +0700

    add sample

--- sample/src/main/kotlin/com/aafanasev/chucknorrissample/Main.kt
+++ sample/src/main/kotlin/com/aafanasev/chucknorrissample/Main.kt
@@ -3,5 +3,26 @@ package com.aafanasev.chucknorrissample
 import com.aafanasev.chucknorris.ChuckNorrisApiClient
 
 fun main(args: Array<String>) {
-	println("Hello World " + ChuckNorrisApiClient.test())
+
+    println("\n--- all categories ---\n")
+
+    ChuckNorrisApiClient.getCategories().subscribe { categories -> println(categories) }
+
+    println("\n--- random joke ---\n")
+
+    ChuckNorrisApiClient.getRandomJoke().subscribe { randomJoke -> println(randomJoke.value) }
+
+    println("\n--- random joke from category ---\n")
+
+    ChuckNorrisApiClient.getRandomJoke("dev").subscribe { randomJoke -> println(randomJoke.value) }
+
+    println("\n--- search jokes ---\n")
+
+    ChuckNorrisApiClient.searchJokes("java").subscribe { result ->
+        println("""
+            Found: ${result.total}
+            First joke: ${result.jokes[0].value}
+            """.trimIndent())
+    }
+
 }
