e788ebdb9ce7fdb3d97ce1724a3c01fab581d23d
jokuskay <tafanasyev@gmail.com>
2018-04-02 03:02:47 +0700

    improve java compatibility

--- library/src/main/kotlin/com/aafanasev/chucknorris/ChuckNorrisApiService.kt
+++ library/src/main/kotlin/com/aafanasev/chucknorris/ChuckNorrisApiService.kt
@@ -13,6 +13,9 @@ interface ChuckNorrisApiService {
     fun getCategories(): Single<List<Category>>
 
     @GET("random")
-    fun getRandomJoke(@Query("category") category: Category? = null): Single<Joke>
+    fun getRandomJoke(): Single<Joke>
+
+    @GET("random")
+    fun getRandomJoke(@Query("category") category: Category): Single<Joke>
 
 }
