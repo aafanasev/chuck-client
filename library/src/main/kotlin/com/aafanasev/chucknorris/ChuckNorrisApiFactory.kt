fd724c4713da8b2cdf89dc2e65498187c5724f6c
jokuskay <tafanasyev@gmail.com>
2018-05-15 23:33:10 +0700

    apply kson library

--- library/src/main/kotlin/com/aafanasev/chucknorris/ChuckNorrisApiFactory.kt
+++ library/src/main/kotlin/com/aafanasev/chucknorris/ChuckNorrisApiFactory.kt
@@ -1,5 +1,8 @@
 package com.aafanasev.chucknorris
 
+import com.aafanasev.kson.generated.KsonTypeAdapterFactory
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -16,13 +19,26 @@ class ChuckNorrisApiFactory {
 
     /**
      * @param okHttpClient custom OkHttpClient
+     * @param useKson use KsonTypeAdapters
      */
     @JvmOverloads
-    fun create(okHttpClient: OkHttpClient? = null): ChuckNorrisApiService = run {
+    fun create(
+            okHttpClient: OkHttpClient? = null,
+            useKson: Boolean = true
+    ): ChuckNorrisApiService = run {
+
+        val gson = if (useKson) {
+            GsonBuilder()
+                    .registerTypeAdapterFactory(KsonTypeAdapterFactory())
+                    .create()
+        } else {
+            Gson()
+        }
+
         val builder = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
 
         okHttpClient?.let {
             builder.client(it)
--- library/src/main/kotlin/com/aafanasev/chucknorris/Entities.kt
+++ library/src/main/kotlin/com/aafanasev/chucknorris/Entities.kt
@@ -1,9 +1,11 @@
 package com.aafanasev.chucknorris
 
+import com.aafanasev.kson.annotation.Kson
 import com.google.gson.annotations.SerializedName
 
 typealias Category = String
 
+@Kson
 data class Joke(
         @SerializedName("id") val id: String,
         @SerializedName("url") val url: String,
@@ -11,6 +13,7 @@ data class Joke(
         @SerializedName("icon_url") val iconUrl: String
 )
 
+@Kson
 data class SearchResult(
         @SerializedName("total") val total: Int,
         @SerializedName("result") val jokes: List<Joke>
